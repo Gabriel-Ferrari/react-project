b99566bcbe931cc64af3691cf4b3d78b1284da8a
Gabriel-Ferrari <gabrielferrari@imobzi.com>
2023-04-05 19:15:40 -0300

    first commit

---
+++ src/components/postCard/index.jsx
@@ -0,0 +1,9 @@
+export const PostCard = ({post}) => {
+    return (
+        <div className="post">
+            <img src={post.cover} alt={post.title} />
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+        </div>
+    )
+}
